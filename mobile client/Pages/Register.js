8b86818648418e033e363a733a49df7c74bd4fae
Aaryan-Thakur <aaryan3120@gmail.com>
2023-01-28 08:37:31 +0530

    before builder X

--- mobile client/Pages/Register.js
+++ mobile client/Pages/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { StyleSheet, Text, TextInput, View,ScrollView} from 'react-native';
 import { Button } from 'react-native-paper';
 import { CommonActions } from '@react-navigation/native';
@@ -6,6 +6,16 @@ import DatePicker from 'react-native-date-picker';
 
 
 const Register = ({navigation}) => {
+
+  const [pass, changePass] = React.useState('')
+
+  // function setpass(e) {
+  //   changePass(e);
+  //   console.log(e.nativeEvent.text);
+
+  // }
+
+  
 
   function onsubmit() {
     navigation.navigate('Home')
@@ -34,12 +44,12 @@ const Register = ({navigation}) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
     <View style={styles.register}>
-    <TextInput style={styles.text} placeholder='First Name'></TextInput>
-    <TextInput style={styles.text} placeholder='Last Name'></TextInput>
-    <TextInput style={styles.text} placeholder='Email'></TextInput>
-    <TextInput style={styles.text} placeholder='Date of Birth'></TextInput>
-    <TextInput secureTextEntry={true} style={styles.text} placeholder='Password'></TextInput>
-    <TextInput secureTextEntry={true} style={styles.text} placeholder='Confirm password'></TextInput>
+    <TextInput style={styles.text}  maxLength={20} placeholder='First Name'></TextInput>
+    <TextInput style={styles.text}  maxLength={20}  placeholder='Last Name'></TextInput>
+    <TextInput style={styles.text}  maxLength={255} placeholder='Email'></TextInput>
+    <TextInput  keyboardType='numeric' style={styles.text} maxLength={2} placeholder='Age'></TextInput>
+    <TextInput name="pass1" secureTextEntry={true} style={styles.text}  maxLength={64}  placeholder='Password'  value={pass}  onChange={changePass} ></TextInput>
+    <TextInput secureTextEntry={true} style={styles.text}  maxLength={64} placeholder='Confirm password'></TextInput>
     </View>
     <Button style={styles.button} mode="contained" onPress={()=> onsubmit()}>register</Button>
     <View><Text>Already have an account?</Text><Button onPress={()=> noaccount()} >Click here</Button></View>
